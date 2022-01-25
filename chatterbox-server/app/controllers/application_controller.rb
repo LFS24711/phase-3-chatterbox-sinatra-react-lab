657cc368284daa5a02276ffb8f83f145de1403ae
Logan <loganfranklinsmith@hotmail.com>
2022-01-25 10:32:07 -0500

    commit tree d84aed57ae5d10a8f02a1ee5ba5fadb997df3ba4

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -2,5 +2,9 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add routes
+
+  get '/messages' do
+    Message.all.order(:created_at).to_json
+  end
   
 end
